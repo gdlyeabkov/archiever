835fb42ace5e4db76b2ecb85630c71430291b2b4
gdlyeabkov <gdlyeabkov@gmail.com>
2022-03-25 18:01:20 +0300

    add_create_zip

---
+++ Archiver/Dialogs/AddArchieveDialog.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Speech.Synthesis;
+using Aspose.Zip;
+using Aspose.Zip.Saving;
+
+namespace Archiver.Dialogs
+{
+    /// <summary>
+    /// Логика взаимодействия для AddArchieveDialog.xaml
+    /// </summary>
+    public partial class AddArchieveDialog : Window
+    {
+
+        public string archievePath = @"C:\Gleb\archiever\zips\";
+        public string archieveName = "compressed_file.zip";
+        public SpeechSynthesizer debugger;
+        public List<string> data;
+
+        public AddArchieveDialog(List<string> data)
+        {
+            InitializeComponent();
+
+            Initialize(data);
+        
+        }
+
+        public void Initialize(List<string> data)
+        {
+            this.data = data;
+            debugger = new SpeechSynthesizer();
+        }
+
+        private void AddArchieveHandler(object sender, RoutedEventArgs e)
+        {
+            AddArchieve();
+            this.Close();
+        }
+
+        private void CancelHandler(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void HelpHandler(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public void AddArchieve()
+        {
+            archieveName = generalArchieveName.Text;
+            using (FileStream zipFile = File.Open(archievePath + archieveName, FileMode.Create))
+            {
+                using (var archive = new Archive(new ArchiveEntrySettings()))
+                {
+                    foreach (string dataItem in data)
+                    {
+                        FileStream source1 = File.Open(dataItem, FileMode.Open, FileAccess.Read);
+                        string sourceFileName = System.IO.Path.GetFileName(dataItem);
+                        archive.CreateEntry(sourceFileName, source1);
+                    }
+                    archive.Save(zipFile);
+                }
+            }
+        }
+
+        private void SetArchieveDestinationHandler(object sender, RoutedEventArgs e)
+        {
+            SetArchieveDestination();
+        }
+
+        private void SetArchieveDestination()
+        {
+            Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog();
+            sfd.FileName = "Архив.zip";
+            sfd.DefaultExt = ".zip";
+            sfd.Filter = "Zip documents (.zip)|*.zip";
+            bool? res = sfd.ShowDialog();
+            if (res != false)
+            {
+                string source = sfd.FileName;
+                generalArchieveName.Text = System.IO.Path.GetFileName(source);
+                FileInfo fileInfo = new FileInfo(source);
+                DirectoryInfo directoryInfo = fileInfo.Directory;
+                string directoryInfoPath = directoryInfo.FullName;
+                char pathSeparator = System.IO.Path.DirectorySeparatorChar;
+                archievePath = directoryInfoPath + pathSeparator;
+            }
+        }
+
+    }
+}
---
+++ Archiver/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Aspose.Zip;
+using Aspose.Zip.Saving;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Speech.Synthesis;
+
+namespace Archiver
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public String currentPath = @"C:\Gleb\archiever\zipsource";
+        public List<string> selectedSourceFiles;
+        public SpeechSynthesizer debugger;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Initialize();
+        
+        }
+
+        public void Initialize()
+        {
+            debugger = new SpeechSynthesizer();
+            selectedSourceFiles = new List<string>();
+            GetSourceFiles();
+        }
+
+        private void AddArchieveHandler(object sender, RoutedEventArgs e)
+        {
+            OpenAddArchieveDialog();
+        }
+
+        public void OpenAddArchieveDialog()
+        {
+            int countSelectedSourceFiles = selectedSourceFiles.Count;
+            bool isSourceFilesSelected = countSelectedSourceFiles >= 1;
+            if (isSourceFilesSelected)
+            {
+                // string data = selectedSourceFiles[0];
+                Archiver.Dialogs.AddArchieveDialog dialog = new Archiver.Dialogs.AddArchieveDialog(selectedSourceFiles);
+                dialog.Show();
+                dialog.Closed += AddArcheveDialogCloseHandler;
+            }
+        }
+
+        public void GetSourceFiles()
+        {
+            string[] sourceFilesPaths = Directory.GetFiles(currentPath);
+            foreach (string sourceFilePath in sourceFilesPaths)
+            {
+                StackPanel sourceFile = new StackPanel();
+                sourceFile.Orientation = Orientation.Horizontal;
+                sourceFile.Height = 15;
+                sourceFile.DataContext = sourceFilePath;
+                string sourceFileName = System.IO.Path.GetFileName(sourceFilePath);
+                TextBlock sourceFileNameLabel = new TextBlock();
+                sourceFileNameLabel.Text = sourceFileName;
+                sourceFile.Children.Add(sourceFileNameLabel);
+                sourceFiles.Children.Add(sourceFile);
+                sourceFile.MouseUp += SelectSourceFileHandler;
+            }
+        }
+
+
+        public void SelectSourceFileHandler(object sender, RoutedEventArgs args)
+        {
+            StackPanel sourceFile = ((StackPanel)(sender));
+            string sourceFileData = ((string)(sourceFile.DataContext));
+            if (!((Keyboard.Modifiers & ModifierKeys.Control) > 0))
+            {
+                selectedSourceFiles.Clear();
+                foreach (StackPanel someSourceFile in sourceFiles.Children)
+                {
+                    someSourceFile.Background = System.Windows.Media.Brushes.White;
+                }
+            }
+            sourceFile.Background = System.Windows.Media.Brushes.LightSkyBlue;
+            bool isSelectedSourceFile = selectedSourceFiles.Contains(sourceFileData);
+            bool isNotSelectedSourceFile = !isSelectedSourceFile;
+            if (isNotSelectedSourceFile)
+            {
+                selectedSourceFiles.Add(sourceFileData);
+            }
+            else if ((Keyboard.Modifiers & ModifierKeys.Control) > 0) {
+                sourceFile.Background = System.Windows.Media.Brushes.White;
+                selectedSourceFiles.RemoveAll((string path) => {
+                    return path == sourceFileData;
+                });
+            }
+            int countSelectedSourceFiles = selectedSourceFiles.Count;
+            string rawCountSelectedSourceFiles = countSelectedSourceFiles.ToString();
+        }
+
+        private void AddArcheveDialogCloseHandler(object sender, EventArgs e)
+        {
+            ClearSelection();
+        }
+
+        public void ClearSelection ()
+        {
+            selectedSourceFiles.Clear();
+            foreach (StackPanel someSourceFile in sourceFiles.Children)
+            {
+                someSourceFile.Background = System.Windows.Media.Brushes.White;
+            }
+        }
+
+    }
+}
